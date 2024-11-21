4a2abfa26aeb50c9d4e7b3f9722424e25249590b
Dhruv Moorthy <dmoorthy@usloaner-mba01.fwparker.org>
2024-11-21 10:19:37 -0600

    Recipe book

---
+++ SPANISH4GAME/Script2.js
@@ -0,0 +1,9 @@
+function generate() {
+    //Insert user inputs into the madlib
+    const a1 = document.getElementById("a1input").value;
+    const n1 = document.getElementById("n1input").value;
+    
+    //Insert user inputs into the madlib
+    document.getElementById("a1").innerText = a1;
+    document.getElementById("n1").innerText = n1;
+}
---
+++ SPANISH4GAME/script.js
@@ -0,0 +1,15 @@
+//const my_name = “Ms. Ifft”;
+//        let age = 67;
+//        console.log(myName);
+//        console.log(age);
+//
+//
+//        const my_name = “Dhruv”;
+//        Let age = 16;
+//
+//console.log(“Hi my name is “, myName, “ and my age is “, age);
+
+
+function reveal(){
+    alert("This works!");
+}
