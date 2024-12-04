85e9b9f474d7c169779a2adaa5ae493152467bde
Dhruv Moorthy <dmoorthy@usloaner-mba01.fwparker.org>
2024-12-04 11:06:21 -0600

    recipe

---
+++ Javascript-arrays/Script.js
@@ -0,0 +1,17 @@
+//let tasks = ["do homework", "practice trombone", "study for SAT"];
+//alert(tasks[0]);
+//
+//
+////change the first item
+//tasks[0] = "make dinner";
+//alert(tasks);
+//
+//tasks.push("eat dinner");
+//alert(tasks);
+//
+//tasks.splice([2]);
+//alert(tasks);
+
+let groceries = ["milk", "fruit", "bread", "eggs"];
+groceries.splice(0,1);
+alert(groceries);
