f7ad15098d5f50a3420fcd3ff0a90fb9bfccbb74
Dhruv Moorthy <dmoorthy@usloaner-mba01.fwparker.org>
2024-12-05 14:36:54 -0600

    homework

--- Javascript-arrays/Script.js
+++ Javascript-arrays/Script.js
@@ -12,6 +12,19 @@
 //tasks.splice([2]);
 //alert(tasks);
 
-let groceries = ["milk", "fruit", "bread", "eggs"];
-groceries.splice(0,1);
-alert(groceries);
+//let groceries = ["milk", "fruit", "bread", "eggs"];
+//groceries.splice(0,1);
+//alert(groceries);
+
+let items = ["computer", "notebook", "backpack"];
+items.push("water bottle");
+items.push("phone");
+console.log(items);
+items.pop();
+console.log(items);
+items.splice([2, 1]);
+console.log(items);
+
+let numbers = ["1","5","4","6"];
+number.splice([0,0,"23"]);
+number.splice([1,1,"35"]);
